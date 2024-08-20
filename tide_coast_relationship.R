9a50d78b3038b4bed84a4456285b29e4d9d74d7a
a-u-j <amelia.jeffery@unsw.edu.au>
2024-08-20 16:33:37 +1000

    Updated tide_coast_relationship script

--- tide_coast_relationship.R
+++ tide_coast_relationship.R
@@ -1,10 +1,5 @@
-install.packages("mvabund")
-install.packages("lme4")
-install.packages("DHARMa")
-install.packages("car")
-install.packages("sjPlot")
-install.packages("performance")
-install.packages("cowplot")
+library(mgcv)
+library(effects)
 library(performance)
 library(car)
 library(ggplot2)
@@ -15,6 +10,9 @@ library(lme4)
 library(DHARMa)
 library(sjPlot)
 library(cowplot)
+library(cosinor)
+
+options(scipen = 20)
 
 data_raw <- read.csv("~/GitHub/collar_data_preparation/output/2-cleaned_collar_data.csv")
 coast <- read.csv("~/GitHub/collar_data_preparation/output/covariates/coast_gps.csv")
@@ -22,15 +20,18 @@ coast <- select(coast,"rownum", "coast_dist", "coast_yn")
 tide <- read.csv("~/GitHub/collar_data_preparation/output/covariates/tidal_gps_2023.csv")
 tide <- select(tide, rownum, tideheight_m,tide_cat)
 
-#data <- left_join(data_raw,tide, by = "rownum")
-data <- left_join(data_raw, coast, by = "rownum")
-#data <- data %>% subset(year == "2023")
+data <- data_raw %>%
+  left_join(tide, by = "rownum") %>%
+  left_join(coast, by = "rownum")
 
-##### Filtering data for close inspection #####
+data_2023 <- data %>% subset(year == "2023") # Only relevant if looking at tide
+
+##### (Can ignore) Filtering data for closer inspection #####
 filtered_data <- data %>%
   mutate(coast_dist_km = coast_dist*0.001) %>%
   subset(id == "UOF1801") %>%
-  filter(day <= 25, month == 5)
+  filter(day <= 25, month == 5) %>%
+  subset(year == "2023")
 
 # Create the plot
 ggplot(filtered_data, aes(x = hour)) +
@@ -55,14 +56,9 @@ ggplot(filtered_data, aes(x = hour)) +
   facet_wrap(~day, nrow = 5, ncol = 5)
 
 
-###### Filtering data for binary variables ######
-# Remove rows with NA values in tide_cat or coast_yn, and filter for coast_yn = "Y"
-data_clean <- data %>%
-  filter(!is.na(tide_cat) & coast_yn == "Y")
-######
 
-##### ggPlot of coast presences by ID in 2023 ####
-# Aggregate the counts of each combination of tide_cat for each month
+##### (Can ignore) ggPlot of coast presences by ID in 2023 ####
+# Aggregate the counts of ID for each month to get average counts of coast binary
 data_summary <- data_clean %>%
   group_by(id, month, coast_yn) %>%
   summarize(count = n(), .groups = 'drop')
@@ -79,21 +75,38 @@ ggplot(data_summary, aes(x = as.factor(month), y = count, fill = coast_yn)) +
   facet_wrap(~id, scales = "free_y") +
   theme_cowplot(12)
 
-######
 
-####### Analysis #######
 
-# data_clean <- data %>%
-#   filter(!is.na(tide_cat) & !is.na(coast_yn))
-
-data_clean <- data %>%
+##### Cleaning data for analysis ####
+data_clean_2023 <- data_2023 %>%
   mutate(coast_yn = as.factor(coast_yn),
-         #tide_cat = as.factor(tide_cat),
+         tide_cat = as.factor(tide_cat),
          month = as.factor(month),
          id = as.factor(id))
 
-###### MODEL 1 #####
-model <- glmer(coast_yn ~ tide_cat + month + (1 | id), data = data_clean, family = binomial())
+data_clean <- data %>%
+  mutate(coast_yn = as.factor(coast_yn),
+         tide_cat = as.factor(tide_cat),
+         month = as.factor(month),
+         id = as.factor(id))
+
+##### Statistical test of difference
+
+data_receding <- data_clean %>% 
+  filter(tide_cat == "receding") %>% 
+  pull(coast_dist)
+data_rising <- data_clean %>% 
+  filter(tide_cat == "rising") %>% 
+  pull(coast_dist)
+
+# Check normality
+ks_test_result <- ks.test(data_receding, data_rising)
+wilcox_test_result <- wilcox.test(data_receding, data_rising)
+
+
+###### MODEL 1 <- TIDE, 2023 ####
+
+model <- glmer(coast_yn ~ tide_cat + (1 | id), data = data_clean_2023, family = binomial())
 summary(model)
 
 vif(model)
@@ -106,9 +119,10 @@ plotResiduals(m, form = data_clean$month)
 testDispersion(m)
 
 chisq.test(table(data_clean$tide_cat, data_clean$month))
-######
 
-##### MODEL 2
+
+
+##### MODEL 2 <- ONLY MONTH ####
 
 model2 <- glmer(coast_yn ~ month + (1 | id), data = data_clean, 
                 family = binomial())
@@ -123,37 +137,38 @@ plotResiduals(m2)
 plotResiduals(m2, form = data_clean$month)
 testDispersion(m2)
 
-######
-
-##### MODEL 3 ######
-model3 <- lmer(coast_dist ~ tide_cat + month + (1 | id), data = data)
-summary(model3)
-
-m3 <- simulateResiduals(model3, plot = TRUE)
-plot(m3)
-######
+#Performance
+check_overdispersion(model2)
+check_convergence(model2)
 
 # Plotting model:
-
 #sjPlot:
-
 sjPlot::plot_model(model2, sort.est = FALSE,
                    show.values = TRUE, show.p = TRUE, value.offset = .3,
                    title = "Presence on coast as function of month",
-                   vline.color = "grey")
+                   vline.color = "grey",
+                   group.terms = c(1,2,2,2,3,3,3,4,4,4,1)
+                   ) + theme_classic()
 
 
 sjPlot::tab_model(model2, show.re.var=TRUE,
                   dv.labels = "Presence on coast as function of month")
-                  
-
-#Performance
-
-check_overdispersion(model2)
-check_convergence(model2)
 
 
-######
+# Further visualising
+
+hours <- seq(0, 23, by = 1)
+cos_hour <- cos(2 * pi * hours / 24)
+sin_hour <- sin(2 * pi * hours / 24)
+month <- factor(rep(1, length(hours)), levels = levels(data_clean$month))
+
+newdata <- data.frame(month = month, cos_hour = cos_hour, sin_hour = sin_hour)
+predicted_probs <- predict(model2, newdata = newdata, type = "response", re.form = NA)
+plot(hours, predicted_probs, type = "l", xlab = "Hour of Day", ylab = "Predicted Probability of Coastal Presence")
 
 
-states <- read.csv()
+##### MODEL 3 <- TIDE, Coast Dist - didn't use ######
+
+model3 <- lmer(coast_dist ~ tide_cat + (1 | id), data = data_clean_2023)
+summary(model3)
+
